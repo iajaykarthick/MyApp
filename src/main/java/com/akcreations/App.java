7bf819b1c1d916f8e18581c8ae5a92bc70d435ac
AjayKarthickS <ajaykarthick483@gmail.com>
2019-07-06 19:48:58 +0530

    First Commit

---
+++ src/main/java/com/akcreations/App.java
@@ -0,0 +1,13 @@
+package com.akcreations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello jenkins from Maven project!" );
+    }
+}
